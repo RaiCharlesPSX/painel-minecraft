4d3a9f13ce812c0ea7c1bae6444defd6b87c5728
RaiCharlesPSX <raicharlespsx@gmail.com>
2025-04-04 17:13:06 -0300

    Add files via upload
    Signed-off-by: RaiCharlesPSX <raicharlespsx@gmail.com>

---
+++ script.js
@@ -0,0 +1,15 @@
+
+function clearSearch() {
+  document.getElementById('search').value = '';
+}
+
+document.getElementById('upload').addEventListener('change', function(event) {
+  const file = event.target.files[0];
+  if (!file) return;
+
+  const img = document.createElement('img');
+  img.src = URL.createObjectURL(file);
+  img.onload = () => URL.revokeObjectURL(img.src);
+  document.getElementById('images').innerHTML = '';
+  document.getElementById('images').appendChild(img);
+});
